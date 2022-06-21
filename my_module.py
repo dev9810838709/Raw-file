bdd12dd7b5e30140aa4ebfc7856a2dfe138f6d9d
dev9810838709 <107929282+dev9810838709@users.noreply.github.com>
2022-06-21 18:04:05 +0530

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,7 @@
+data = {"name":"ineorun" , "course":["data science","blockchain","drone","robotics"],"greeting":"greeting from ineoron"}
+
+def get_course():
+	return data["course"]
+
+def greetings():
+	return data["greeting"]
